4d04d075f0bbcd63fccdb1b4382e3cbbe1bcb939
CharBriss12 <charlotbriss12@gmail.com>
2024-04-27 18:16:24 +0300

    creating a string for the project

---
+++ string/string.h
@@ -0,0 +1,37 @@
+#ifndef LABORATORY_18_STRING_H
+#define LABORATORY_18_STRING_H
+
+#define CHAR_SIZE sizeof(char)
+#define MAX_STRING_SIZE 100
+#define MAX_N_WORDS_IN_STRING 100
+#define MAX_WORD_SIZE 20
+
+static char string_buffer[MAX_STRING_SIZE + 1];
+
+#include <memory.h>
+#include <stdbool.h>
+
+size_t getLength(const char* string);
+
+char* find(char* start, const char* end, int character);
+
+char* findNonSpace(char* string);
+
+char* findSpace(char* string);
+
+char* findNonSpaceReverse(const char* searchEnd, char* searchStart);
+
+char* findSpaceReverse(const char* searchEnd, char* searchStart);
+
+int compareStrings(char* left, char* right);
+
+char* copy(const char* start, const char* end, char* destination);
+
+char* copyIf(char* start, const char* end, char* destination, bool (*filter)(char));
+
+char* copyIfReverse(char* reversedStart, const char* reversedEnd, char* destination, bool (*filter)(char));
+
+char* reverse(const char* begin, const char* end);
+
+
+#endif //LABORATORY_18_STRING_H
